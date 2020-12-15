09dc0adbd87c00b7a52e8d37b0daa8ba52fb4fc4
Alex <Oleksandr.Piatkin@gmx.com>
2020-12-15 22:29:24 +0100

    Color change fixed

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-let css = document.querySelector("h3");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let body = document.getElementById("gradient");
-let randomButton = document.querySelector(".button");
+const css = document.querySelector("h3");
+const leftColor = document.getElementById("color1");
+const rightColor =  document.getElementById("color2");
+const body = document.getElementById("gradient");
+const randomButton = document.querySelector(".button");
+
+leftColor.addEventListener("input", setGradient);
+rightColor.addEventListener("input", setGradient);
+randomButton.addEventListener("click", setRandomColors);
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
+	body.style.background = `linear-gradient(to right, ${leftColor.value}, ${rightColor.value})`;
 	css.textContent = body.style.background + ";";
 }
-
 setGradient();
 
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
-randomButton.addEventListener("click", randomColors);
+function getRandomColor() {
+	const randomColor = parseInt(Math.random() * 0xffffff).toString(16);
 
-function randomColors() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ '#' + parseInt(Math.random() * 0xffffff).toString(16) 
-	+ ", " 
-	+ '#' + parseInt(Math.random() * 0xffffff).toString(16)
-	+ ")";
+	if (randomColor.length === 6) {
+		return randomColor;
+	} else {
+		return getRandomColor();
+	}
+}
+
+function setRandomColors() {
+	const left = `#${getRandomColor()}`;
+	const right = `#${getRandomColor()}`;
+
+	body.style.background = `linear-gradient(to right, ${left}, ${right})`;
 	css.textContent = body.style.background + ";";
+	
+	leftColor.value = left;
+	leftColor.defaultValue = left;
+	rightColor.value = right;
+	rightColor.defaultValue= right;
 }
